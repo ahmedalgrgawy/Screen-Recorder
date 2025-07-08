142bdba5f9147519ccb82cafafad4565d5888306
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2025-07-08 19:54:22 +0300

    update login - arcjet files

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -1,18 +1,30 @@
-import aj from "@/lib/arcjet";
+
+import aj, {
+    ArcjetDecision,
+    shield,
+    slidingWindow,
+    validateEmail,
+} from "@/lib/arcjet";
+import ip from "@arcjet/ip";
 import { auth } from "@/lib/auth";
-import { ArcjetDecision, shield, shield, slidingWindow, validateEmail } from "@arcjet/next";
 import { toNextJsHandler } from "better-auth/next-js";
 import { NextRequest } from "next/server";
-import ip from "@arcjet/ip";
-
 
 const emailValidation = aj.withRule(
-    validateEmail({ mode: "LIVE", block: ["DISPOSABLE", "INVALID", "NO_MX_RECORDS"] })
-)
+    validateEmail({
+        mode: "LIVE",
+        block: ["DISPOSABLE", "INVALID", "NO_MX_RECORDS"],
+    })
+);
 
 const rateLimit = aj.withRule(
-    slidingWindow({ mode: "LIVE", interval: "2m", max: 2, characteristics: ['fingerprint'] })
-)
+    slidingWindow({
+        mode: "LIVE",
+        interval: "2m",
+        max: 2,
+        characteristics: ["fingerprint"],
+    })
+);
 
 const shieldValidation = aj.withRule(
     shield({
@@ -22,39 +34,33 @@ const shieldValidation = aj.withRule(
 
 const protectedAuth = async (req: NextRequest): Promise<ArcjetDecision> => {
     const session = await auth.api.getSession({
-        headers: req.headers()
-    })
-
+        headers: req.headers,
+    });
     let userId: string;
-
-    if (session?.user?.id) {
-        userId = session.user.id
+    if (session?.user.id) {
+        userId = session.user.id;
     } else {
-        userId = ip(req) || '127.0.0.1'
+        userId = ip(req) || "127.0.0.1";
     }
-
     if (req.nextUrl.pathname.startsWith("/api/auth/sign-in")) {
-        const body = await req.clone().json()
-
+        const body = await req.clone().json();
         if (typeof body.email === "string") {
             return emailValidation.protect(req, {
                 email: body.email,
             });
         }
     }
-
     if (!req.nextUrl.pathname.startsWith("/api/auth/sign-out")) {
         return rateLimit.protect(req, {
             fingerprint: userId,
         });
     }
     return shieldValidation.protect(req);
+};
 
-}
+const authHandlers = toNextJsHandler(auth.handler);
 
-const authHandler = toNextJsHandler(auth.handler)
-
-export const { GET } = authHandler
+export const { GET } = authHandlers;
 
 export const POST = async (req: NextRequest) => {
     const decision = await protectedAuth(req);
@@ -70,5 +76,5 @@ export const POST = async (req: NextRequest) => {
         }
     }
 
-    return authHandler.POST(req);
+    return authHandlers.POST(req);
 };
