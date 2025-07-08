496ffc8b6cbd7b1cc2f3f04e88c4efb697d4dcab
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2025-07-08 20:23:58 +0300

    Diplay Videos In Home Page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,31 +1,53 @@
+import EmptyState from '@/components/EmptyState'
 import Header from '@/components/Header'
+import Pagination from '@/components/Pagination'
 import VideoCard from '@/components/VideoCard'
-import { dummyCards } from '@/constants'
+import { getAllVideos } from '@/lib/actions/video'
 import React from 'react'
 
-const Page = () => {
+const Page = async ({ searchParams }: SearchParams) => {
+
+  const { query, filter, page } = await searchParams
+
+  const { videos, pagination } = await getAllVideos(query, filter, Number(page) || 1)
+
   return (
     <main className="wrapper page">
       <Header title='All Videos' subHeader='Public Library' />
 
-      <section className="video-grid">
-        {dummyCards.map((card) => {
-          return (
+      {videos?.length > 0 ? (
+        <section className='video-grid'>
+          {videos.map(({ video, user }) => (
             <VideoCard
-              key={card.id}
-              id={card.id}
-              title={card.title}
-              thumbnail={card.thumbnail}
-              createdAt={card.createdAt}
-              userImg={card.userImg}
-              username={card.username}
-              views={card.views}
-              visibility={card.visibility}
-              duration={card.duration}
+              key={video.id}
+              id={video.videoId}
+              title={video.title}
+              thumbnail={video.thumbnailUrl}
+              createdAt={video.createdAt}
+              userImg={user?.image ?? ""}
+              username={user?.name ?? "Guest"}
+              views={video.views}
+              visibility={video.visibility}
+              duration={video.duration}
             />
-          )
-        })}
-      </section>
+          ))}
+        </section>
+      ) : (
+        <EmptyState
+          icon="/assets/icons/video.svg"
+          title="No Videos Found"
+          description="Try adjusting your search."
+        />
+      )}
+
+      {pagination?.totalPages > 1 && (
+        <Pagination
+          currentPage={pagination.currentPage}
+          totalPages={pagination.totalPages}
+          queryString={query}
+          filterString={filter}
+        />
+      )}
 
     </main>
   )
