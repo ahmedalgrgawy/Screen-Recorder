6d16b579b5066333ae09820a0e8e2ebd33cd2a6b
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2025-07-09 16:24:29 +0300

    User Profile & Videos

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,36 +1,50 @@
-import Header from "@/components/Header";
+import { redirect } from "next/navigation";
+import { getAllVideosByUser } from "@/lib/actions/video";
+import EmptyState from "@/components/EmptyState";
 import VideoCard from "@/components/VideoCard";
-import { dummyCards } from "@/constants";
+import SharedHeader from "@/components/SharedHeader";
 
-const Page = async ({ params }: ParamsWithSearch) => {
-
+const ProfilePage = async ({ params, searchParams }: ParamsWithSearch) => {
     const { id } = await params;
+    const { query, filter } = await searchParams;
+
+    const { user, videos } = await getAllVideosByUser(id, query, filter);
+    if (!user) redirect("/404");
 
     return (
-        <div className="wrapper page">
-            <Header subHeader="Profile" title="User Profile" userImg="/assets/images/dummy.jpg" />
+        <main className="wrapper page">
+            <SharedHeader
+                subHeader={user?.email}
+                title={user?.name}
+                userImg={user?.image ?? ""}
+            />
 
-            <section className="video-grid">
-                {dummyCards.map((card) => {
-                    return (
+            {videos?.length > 0 ? (
+                <section className="video-grid">
+                    {videos.map(({ video }) => (
                         <VideoCard
-                            key={card.id}
-                            id={card.id}
-                            title={card.title}
-                            thumbnail={card.thumbnail}
-                            createdAt={card.createdAt}
-                            userImg={card.userImg}
-                            username={card.username}
-                            views={card.views}
-                            visibility={card.visibility}
-                            duration={card.duration}
+                            key={video.id}
+                            id={video.videoId}
+                            title={video.title}
+                            thumbnail={video.thumbnailUrl}
+                            createdAt={video.createdAt}
+                            userImg={user.image ?? ""}
+                            username={user.name ?? "Guest"}
+                            views={video.views}
+                            visibility={video.visibility}
+                            duration={video.duration}
                         />
-                    )
-                })}
-            </section>
+                    ))}
+                </section>
+            ) : (
+                <EmptyState
+                    icon="/assets/icons/video.svg"
+                    title="No Videos Available Yet"
+                    description="Video will show up here once you upload them."
+                />
+            )}
+        </main>
+    );
+};
 
-        </div>
-    )
-}
-
-export default Page;
+export default ProfilePage;
--- components/DropdownList.tsx
+++ components/DropdownList.tsx
@@ -1,38 +1,54 @@
-"use client"
+"use client";
+
 import Image from "next/image";
 import { useState } from "react";
 
-const DropdownList = () => {
+import { cn } from "@/lib/utils";
 
+const DropdownList = ({
+    options,
+    selectedOption,
+    onOptionSelect,
+    triggerElement,
+}: DropdownListProps) => {
     const [isOpen, setIsOpen] = useState(false);
+
+    const handleOptionClick = (option: string) => {
+        onOptionSelect(option);
+        setIsOpen(false);
+    };
 
     return (
         <div className="relative">
             <div className="cursor-pointer" onClick={() => setIsOpen(!isOpen)}>
-                <div className="filter-trigger">
-                    <figure>
-                        <Image src="/assets/icons/hamburger.svg" alt="menu" width={14} height={14} />
-                        Most Recent
-                    </figure>
-                    <Image src="/assets/icons/arrow-down.svg" alt="arrow down" width={20} height={20} />
-                </div>
-
-                {isOpen && (
-                    <ul className="dropdown">
-
-                        {["Most Recent", "Most Liked"].map((item, index) => (
-                            <li key={index} className="list-item">
-                                <button onClick={() => setIsOpen(false)}>
-                                    {item}
-                                </button>
-                            </li>
-                        ))}
-
-                    </ul>
-                )}
+                {triggerElement}
             </div>
-        </div>
-    )
-}
 
-export default DropdownList
+            {isOpen && (
+                <ul className="dropdown">
+                    {options.map((option) => (
+                        <li
+                            key={option}
+                            className={cn("list-item", {
+                                "bg-pink-100 text-white": selectedOption === option,
+                            })}
+                            onClick={() => handleOptionClick(option)}
+                        >
+                            {option}
+                            {selectedOption === option && (
+                                <Image
+                                    src="/assets/icons/check.svg"
+                                    alt="check"
+                                    width={16}
+                                    height={16}
+                                />
+                            )}
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
+};
+
+export default DropdownList;
